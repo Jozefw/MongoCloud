7b026ed106ca74ac0ef160fc6615566b9fd313a0
Jozef <Jozefw@sbcglobal.net>
2024-01-24 10:53:34 -0800

    add cart functionality to users model

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const mongoDB = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(username,email){
+  constructor(username,email,cart,id){
     this.username = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save(){
@@ -19,6 +21,20 @@ class User {
     })
     }
 
+    addToCart(product){
+      // const cartContents = this
+      // .cart
+      // .items
+      // .findIndex(cartItems =>{
+      //   return cartItems._id === product._id
+      // })
+      const updatedCart = {items: [{...product, quantity:1}]}
+      const DB = getDb();
+      return DB
+      .collection('users')
+      .updateOne({_id:new mongoDB.ObjectId(this._id)},
+      {$set:{cart:updatedCart}})
+    }
 
   static findById(userId){
     const DB = getDb();
@@ -26,6 +42,7 @@ class User {
     .findOne({_id:new mongoDB.ObjectId(userId)})
     .then((results)=>{
       console.log("get username",results)
+      return results;
     })
     .catch((err)=>{
       console.log("user insertion",err)
